b4168d143847ededf75500552f9cae8ead9a7c21
Hamed Sargoli <41745585+hamedsargoli@users.noreply.github.com>
2023-02-16 20:32:53 +0330

    14011127

--- Example Function/String/islower.py
+++ Example Function/String/islower.py
@@ -6,30 +6,40 @@ Created on Sun Feb  14 23:11:45 2023
 @author: Hamed Sargoli
 """
 
-print()
-print('Example :\n')
 
-#ex1:    
-a = 'hi'
-print( 'hi'.islower() , '\n')
+#ex1:   
+print('\n' , 'ex1:')    
+
+result = 'Hi'.islower()
+
+print( result , '\n')
+
 
 #ex2:
-a = 'HI'
-print(f'2. Is {a !r} upper? :' , a.islower() , '\n')
+a = 'hi'
+print(f'ex2. Is {a !r} lower? :' , a.islower() , '\n')
 
 #ex3:
 a = 'HI 2.2'
-print(f'3. Is {a !r} upper? :' , a.islower() , '\n')
+print(f'ex3. Is {a !r} lower? :' , a.islower() , '\n')
 
 #ex4:
-a = '5 # HI'
-print(f'4. Is {a !r} upper? :' , a.islower() , '\n')
+a = '5 # hi'
+print(f'ex4. Is {a !r} lower? :' , a.islower() , '\n')
 
-#ex5: find the number of upper characters in the text(a)
-a = "python IS a PROGRAMMING language."
-#a = "python 3.03 $$$"
-count=0
-for i in a:
-    if i.islower():
-        count += 1
-print(f'5. The {count} character is Lower.')
+#ex5: How to use islower() in a program?
+print('ex5:')
+s = 'this is good'
+if s.islower() == True:
+  print('Does not contain uppercase letter.')
+else:
+  print('Contains uppercase letter.')
+
+#ex6: How to use islower() in a program?  
+print('ex6:')
+s = 'this is Good'
+if s.islower():
+  print('Does not contain uppercase letter.')
+else:
+  print('Contains uppercase letter.')
+
