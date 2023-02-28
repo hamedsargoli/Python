39abbd0970aca50b0a40a6e0dd6d64b7618b0262
Hamed Sargoli <41745585+hamedsargoli@users.noreply.github.com>
2023-02-28 19:13:04 +0330

    Add Capitalize Example
    14011209

---
+++ Example Function/String/capitalize.py
@@ -0,0 +1,39 @@
+# capitalize() Function
+"""
+Created on Fri Feb 24 19:57:21 2023
+
+@author: Hamed Sargoli
+
+"""
+
+
+#----------------------------------------------------------------
+#Example 1
+print('\nExample 1:')
+var="hi"
+print(' orginal string is    : ' , var)
+print(' capitalize string is : ' , var.capitalize())
+
+
+#----------------------------------------------------------------
+#Example 2
+print('\nExample 2:')
+var="hamed SARGOLI"
+print(' orginal string is    : ' , var)
+print(' capitalize string is : ' , var.capitalize())
+
+
+#----------------------------------------------------------------
+#Example 3
+print('\nExample 3:')
+var="@Hamed.sargoli@GMAIL.com"
+print(' orginal string is    : ' , var)
+print(' capitalize string is : ' , var.capitalize())
+
+
+#----------------------------------------------------------------
+#Example 4
+print('\nExample 4:')
+var="36 is my age."
+print(' orginal string is    : ' , var)
+print(' capitalize string is : ' , var.capitalize())
